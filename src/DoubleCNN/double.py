d70cc95fad2f0c6484658c18c2075ce73d2735cc
shintaroDesu <snkraishin87@gmail.com>
2024-05-01 18:44:45 +0200

    update double

--- src/DoubleCNN/double.py
+++ src/DoubleCNN/double.py
@@ -149,7 +149,8 @@ if __name__ == '__main__':
 
     model = Baseline(config)
 
-    checkpoint_callback = ModelCheckpoint(dirpath="../../checkpoints/DoubleCNN/", save_on_train_epoch_end=True)
+    checkpoint_callback = ModelCheckpoint(dirpath="../../checkpoints/DoubleCNN/", every_n_train_steps=2, save_top_k=1, save_last=True,
+                                          monitor="train_loss", mode="min")
     checkpoint_callback.CHECKPOINT_NAME_LAST = yamlfile.name
 
     trainer = pl.Trainer(max_epochs=config.epoch,

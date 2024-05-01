d498375d61faa67191f3999791ec4882b84ba3a0
shintaroDesu <snkraishin87@gmail.com>
2024-05-01 18:04:08 +0200

    forking double cnn

---
+++ src/DoubleCNN/double.py
@@ -0,0 +1,181 @@
+import pytorch_lightning as pl
+import torch.nn as nn
+import torch
+import argparse
+import yaml
+import munch
+from pytorch_lightning.callbacks import ModelCheckpoint
+import wandb
+import torchinfo
+import os
+import json
+from torchvision.models import efficientnet_b7
+
+
+class VideoDataset(torch.utils.data.Dataset):
+    def __init__(self, config, metadata_path):
+        self.folder_path = "/raid/home/automathon_2024/account24/data/processed3"
+        metadata = json.load(open(metadata_path))
+        self.filename = [f"{k[:-4]}.pt" for k in metadata.keys()]
+
+        # remove files that do not exist
+        filelist = []
+        for f in self.filename:
+            if os.path.exists(f"{self.folder_path}/{f}"):
+                filelist.append(f)
+
+        self.filename = filelist
+
+        self.labels = [1 if metadata[v] == 'fake' else 0 for v in metadata.keys()]
+
+        print(sum(self.labels)/len(self.labels))
+
+        self.config = config
+
+    def __len__(self):
+        return len(self.filename)
+
+    def __getitem__(self, idx):
+        filename = self.filename[idx]
+        x = torch.load(f'{self.folder_path}/{filename}')
+        start_middle = x.shape[1] // 2 - self.config.n_frames // 2
+        x = x[:, start_middle:start_middle + self.config.n_frames]
+        y = self.labels[idx]
+
+        if x.shape[1] != self.config.n_frames:
+            print(x.shape[1], filename)
+            return self.__getitem__((idx+1) % len(self))
+
+        return x.float()/255.0, y
+
+
+class CNN3d(nn.Module):
+    def __init__(self, channel_list):
+        super().__init__()
+
+        self.conv1 = nn.Conv3d(channel_list[0], channel_list[1], kernel_size=3, padding=1)
+        self.conv2 = nn.Conv3d(channel_list[1], channel_list[2], kernel_size=3, padding=1)
+        self.conv3 = nn.Conv3d(channel_list[2], channel_list[3], kernel_size=3, padding=1)
+        self.conv4 = nn.Conv3d(channel_list[3], channel_list[4], kernel_size=3, padding=1)
+        self.conv5 = nn.Conv3d(channel_list[4], channel_list[5], kernel_size=3, padding=1)
+        self.pool = nn.MaxPool3d(2)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm3d(channel_list[1])
+        self.bn2 = nn.BatchNorm3d(channel_list[2])
+        self.bn3 = nn.BatchNorm3d(channel_list[3])
+        self.bn4 = nn.BatchNorm3d(channel_list[4])
+
+    def forward(self, x):
+        print("1",x.shape,"\n")
+        x = self.bn1(self.conv1(x))
+        print("2", x.shape,"\n")
+        x = self.relu(x)
+        x = self.pool(x)
+        print("3", x.shape,"\n")
+        x = self.bn2(self.conv2(x))
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.bn3(self.conv3(x))
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.bn4(self.conv4(x))
+        x = self.relu(x)
+        x = self.pool(x)
+        x = self.relu(self.conv5(x))
+        print("4", x.shape, "\n")
+        return x
+
+
+class PredictionHead(nn.Module):
+    def __init__(self):
+        super(PredictionHead, self).__init__()
+        self.linear1 = nn.LazyLinear(256)
+        self.linear2 = nn.Linear(256, 1)
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
+        print("shape", x.shape)
+        x = self.flatten(x)
+        x = self.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x.squeeze()
+
+
+class Baseline(pl.LightningModule):
+    def __init__(self, config):
+        super(Baseline, self).__init__()
+        self.config = config
+
+        self.model = efficientnet_b7(pretrained=True)
+        self.model = nn.Sequential(*list(self.model.children())[:-1])
+        #self.model = CNN3d(config.channels)
+        self.head = PredictionHead()
+        self.loss = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(0.3))
+
+    def forward(self, x):
+        x = x[:,:,0,:,:]
+        return self.head(self.model(x))
+
+    def training_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.loss(y_hat, y.float())
+
+        self.log("train_loss", loss, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = self.loss(y_hat, y.float())
+
+        self.log("val_loss", loss, prog_bar=True)
+
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.config.lr)
+
+
+if __name__ == '__main__':
+
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', type=str, default="/raid/home/automathon_2024/account24/erwin/automathon-2024/configs/CNNErwin/config.yaml")
+
+    args = parser.parse_args()
+    with open(args.config_path, 'r') as f:
+        yamlfile = munch.munchify(yaml.safe_load(f))
+    config = yamlfile.config
+
+
+    # model = CNN3d(config.channels)
+    # torchinfo.summary(model, (1, 3, config.n_frames, 128, 128))
+
+    logger = pl.loggers.WandbLogger(project="Deepfake challenge", config=config, group=yamlfile.name, entity="automathon")
+
+    model = Baseline(config)
+
+    checkpoint_callback = ModelCheckpoint(dirpath="/raid/home/automathon_2024/account24/erwin/automathon-2024/checkpoints/CNNErwin/", every_n_epochs=1, save_top_k=-1)
+                                          #every_n_train_steps=2, save_top_k=1, save_last=True, 
+                                          #monitor="val_loss", mode="min")
+    checkpoint_callback.CHECKPOINT_NAME_LAST = yamlfile.name
+
+    trainer = pl.Trainer(max_epochs=config.epoch,
+                         accelerator="auto",
+                         precision='16-mixed',
+                         callbacks=[checkpoint_callback],
+                         logger=logger,)
+
+    train_dataset = VideoDataset(config, "/raid/home/automathon_2024/account24/data/metadata.json")
+    val_dataset = VideoDataset(config, "/raid/home/automathon_2024/account24/data/metadata_info.json")
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=config.batch_size, shuffle=True, num_workers=2)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=config.batch_size, num_workers=2)
+
+    trainer.fit(model, train_loader, val_loader)
+
+
---
+++ src/DoubleCNN/inference.py
@@ -0,0 +1,60 @@
+import argparse
+import torch
+import torch.nn as nn
+from double import Baseline
+import yaml
+import munch
+import json
+import pandas as pd
+import os
+
+
+if __name__ == '__main__':
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path', type=str, default="/raid/home/automathon_2024/account24/erwin/automathon-2024/configs/CNNErwin/config.yaml")
+    parser.add_argument('--checkpoint_path', type=str)
+    parser.add_argument('--method', type=str, default="none")
+    args = parser.parse_args()
+
+    with open(args.config_path, 'r') as f:
+        yamlfile = munch.munchify(yaml.safe_load(f))
+    config = yamlfile.config
+
+    model = Baseline.load_from_checkpoint(args.checkpoint_path, config=config)
+    model.eval()
+    model.to(device)
+
+    sample_submission = pd.read_csv("/raid/home/automathon_2024/account24/data/sample_submission.csv") # id, ...
+    datasetcsv = pd.read_csv("/raid/home/automathon_2024/account24/data/dataset.csv")  # id, file
+
+    id_to_file = {row['id']: row['file'] for _, row in datasetcsv.iterrows()}
+
+    for i, row in sample_submission.iterrows():
+        video_id = row['id']
+        file = id_to_file[video_id]
+        file = file[:-4] + ".pt"
+        video_path = f"/raid/home/automathon_2024/account24/data/processed3/{file}"
+
+        if not os.path.exists(video_path):
+            # sample_submission.loc[i, 'label'] = 1
+            continue
+
+        faces = torch.load(video_path)
+        start_middle = faces.size(1) // 2 - config.n_frames//2
+        faces = faces[:, start_middle:start_middle+config.n_frames]
+        faces = faces.unsqueeze(0).half().to(device) / 255.0
+
+        with torch.cuda.amp.autocast():
+            with torch.no_grad():
+                if(args.method == 'mean'):
+                    print("Mean inference")
+                    y_hat = model(faces.permute(2,1,0,3,4)).mean()
+                else:
+                    print('Default inference')
+                    y_hat = model(faces)
+
+        sample_submission.loc[i, 'label'] = y_hat.item()
+        print(f"{i}, {video_id}, {y_hat.item()}")
+
+    sample_submission.to_csv("/raid/home/automathon_2024/account24/erwin/submission.csv", index=False)
